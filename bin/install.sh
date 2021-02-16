d90fce34425730e2bcd3a8264992d7b564b69bab
Basil Stotz <stotz@amxa.ch>
2021-02-16 15:09:11 +0100

    take real hostname

--- bin/install.sh
+++ bin/install.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-test -n "$(which ltsp)" && exit 0
+if test -z "$(which ltsp)";then
+    add-apt-repository ppa:ltsp
+    apt update
+    apt install --install-recommends \
+        ltsp ltsp-binaries dnsmasq nfs-kernel-server\
+        openssh-server squashfs-tools ethtool net-tools epoptes
+    ltsp dnsmasq
+    ltsp image /
+    ltsp ipxe
+    ltsp nfs
+    ltsp initrd
+fi
 
-add-apt-repository ppa:ltsp
-apt update
-apt install --install-recommends \
-    ltsp ltsp-binaries dnsmasq nfs-kernel-server\
-    openssh-server squashfs-tools ethtool net-tools epoptes
-ltsp dnsmasq
-ltsp image /
-ltsp ipxe
-ltsp nfs
-ltsp initrd
-
+sed -e "s/%%hostname%%/$(hostname)/g" -i ./tree/usr/local/bin/ltsp-updater 
 cp -r ./tree/* /
 
 exit 0
