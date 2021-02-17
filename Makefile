cea02c9760cbffd5afff4ce3ba65234e27c6a6a0
Basil Stotz <stotz@amxa.ch>
2021-02-17 13:53:45 +0100

    rewrite with zenity

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
+.PHONY: install
+install: package
+	@find . -name ltsp-updater_\*_amd64.deb -exec dpkg -i \{\} \;
+
+
+
 .PHONY: package
 package:
 	@find . -name ltsp-updater_\*_amd64.deb -exec rm \{\} \;
--- bin/package.sh
+++ bin/package.sh
@@ -12,7 +12,7 @@ Maintainer: Basil Stotz <stotz@amxa.ch>
 Package: $NAME
 Version: $VERSION 
 Architecture: amd64
-Depends: debconf
+Depends: debconf, zenity, lxqt-sudo, dnsmasq, nfs-kernel-server, openssh-server, squashfs-tools, ethtool, net-tools, epoptes
 Description: Setup and maintain a LTSP server on ubuntu
  Setup and maintain a LTSP server on ubuntu
 EOF
