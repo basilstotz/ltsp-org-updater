b67ea6dc9cca9f1ae6c2edfa97650b3f337b2c9b
Basil Stotz <basil.stotz@gmail.com>
2022-08-13 16:12:50 +0200

    *

--- moreusers.sh
+++ moreusers.sh
@@ -3,7 +3,7 @@
 for I in $(seq 10); do
     II=$(printf "%02i\n" $I)
     USER="user-$II"
-    if useradd --create-home --user-group $USER;then
+    if useradd --create-home --user-group --shell /usr/bin/bash $USER;then
 	echo "$USER:$USER" | chpasswd
     fi
 done
