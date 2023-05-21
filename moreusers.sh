8c7ca757c570db1a0377e5b7a6dc39b072f5ca19
Basil Stotz <basil.stotz@gmail.com>
2023-05-21 09:56:04 +0200

    cleanup

--- moreusers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for I in $(seq 10); do
-    II=$(printf "%02i\n" $I)
-    USER="user-$II"
-    if useradd --create-home --user-group --shell /usr/bin/bash $USER;then
-	echo "$USER:$USER" | chpasswd
-    fi
-done
